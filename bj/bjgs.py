0ab73ee3340fc871028aaa85404c139cd70e9f63
xzxytx <392296204@qq.com>
2017-12-10 11:51:30 +0800

    Update bjgs.py

--- bj/bjgs.py
+++ bj/bjgs.py
@@ -315,13 +315,5 @@ class Bjgs():
 #     operation_mysql = OperationMysql()
 
 
-if __name__ == '__main__':
-    bjgs = Bjgs('372924199601063012', 'liu123456', '刘敬', '201')
-    # bjrbj = Bjrbj('110107197906121210', 'cuipeng6120')
-    # bjgs.start()
-
-    bjgs.first('4')
-    # bjgs.second(1)
-    # bjgs.automatic()
 
 
